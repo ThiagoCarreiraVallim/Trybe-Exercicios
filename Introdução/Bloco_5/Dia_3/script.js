6eb53d01eded6b78dffa5cfdb95615870d67d260
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2020-12-05 21:15:08 -0400

    Commit Inicial

---
+++ Introdução/Bloco_5/Dia_3/script.js
@@ -0,0 +1,173 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+// Escreva seu código abaixo.
+function holidays(day) {
+  let holiday = [24, 25, 31];
+  let friday = [4, 11, 18, 25];
+  let classe = 'day';
+  for (let index = 0; index < 5; index += 1) {
+    switch (true) {
+      case (day === holiday[index]):
+        classe += ' holiday';
+        break;
+      case (day === friday[index]):
+        classe += ' friday';
+    }
+  }
+  return classe
+}
+
+function daysMonth() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+  let weekDays = document.querySelector('#days');
+  for (let index of dezDaysList) {
+    let days = document.createElement('li');
+    days.innerText = index
+    days.className = holidays(index);
+    weekDays.appendChild(days);
+  }
+}
+
+function createButtonHoliday(str) {
+  let buttonCreate = document.createElement('button');
+  let buttonsContainer = document.querySelector('.buttons-container');
+  buttonCreate.id = 'btn-holiday';
+  buttonCreate.innerText = str;
+  buttonsContainer.appendChild(buttonCreate);
+}
+
+function onClickHoliday() {
+  let holidayButton = document.querySelectorAll('.holiday');
+  if (holidayButton[0].style.backgroundColor === 'green') {
+    for (let index = 0; index < holidayButton.length; index += 1) {
+      holidayButton[index].style.backgroundColor = 'rgb(238,238,238)';
+      holidayButton[index].style.color = '#666';
+    }
+  } else{
+    for (let index = 0; index < holidayButton.length; index += 1) {
+      holidayButton[index].style.backgroundColor = 'green';
+      holidayButton[index].style.color = 'white';
+    }
+  }
+}
+
+function changeColor () {
+  let button = document.querySelector('#btn-holiday');
+  button.addEventListener('click', onClickHoliday);
+
+}
+
+function createButtonFriday(str) {
+  let buttonCreateFriday = document.createElement('button');
+  let buttonsContainer = document.querySelector('.buttons-container');
+  buttonCreateFriday.id = 'btn-friday';
+  buttonCreateFriday.innerText = str;
+  buttonsContainer.appendChild(buttonCreateFriday);
+}
+
+function onClickFriday() {
+  let fridayButton = document.querySelectorAll('.friday')
+  let backDays = [4, 11, 18, 25];
+  for (index = 0; index < fridayButton.length; index += 1) {
+    if (fridayButton[index].innerText == 'Sextou!') {
+      fridayButton[index].innerText = backDays[index];
+    }else {
+      backDays.push(fridayButton[index]);
+      fridayButton[index].innerText = 'Sextou!'
+
+    }
+  }
+}
+
+function changeFriday() {
+  let buttonFriday = document.querySelector('#btn-friday');
+  buttonFriday.addEventListener('click', onClickFriday);
+}
+
+function mouseOverZoomIn(event) {
+  event.target.style.transform = 'scale(1.6)'
+  event.target.style.background = 'none';
+  event.target.style.transition = '0.5s'
+  event.target.style.cursor = 'pointer'
+  event.target.style.color = '#666'
+}
+
+function mouseOverZoomOut(event) {
+  event.target.style.transform = 'scale(1)';
+  event.target.style.transition = '0.4s'
+  event.target.style.cursor = 'pointer'
+  event.target.style.opacity = '1'
+}
+
+function mouseOutHoliday(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+}
+
+function zoomList() {
+  let listDay = document.querySelectorAll('.day');
+  let listHoliday = document.querySelectorAll('.holiday');
+  for (let index = 0; index < listDay.length; index += 1){
+    listDay[index].addEventListener('mouseover', mouseOverZoomIn);
+    listDay[index].addEventListener('mouseout', mouseOverZoomOut);
+  }
+  for (let index = 0; index < listHoliday.length; index += 1) {
+    listHoliday[index].addEventListener('mouseout', mouseOutHoliday)
+  }
+}
+
+function addTask(task) {
+  let addSpanTaks = document.createElement('span');
+  let divMyTasks = document.querySelector('.my-tasks');
+  addSpanTaks.innerText = task;
+  divMyTasks.appendChild(addSpanTaks);
+}
+
+function addColorTask(color) {
+  let addDivTask = document.createElement('div');
+  let divMyTasks = document.querySelector('.my-tasks');
+  addDivTask.className = 'task';
+  addDivTask.style.backgroundColor = color;
+  divMyTasks.appendChild(addDivTask);
+}
+
+function isSelected() {
+  let taskSelected = document.querySelector('div.my-tasks div');
+  let classTask = taskSelected.className;
+  if (classTask === 'task-selected') {
+    taskSelected.className = 'task'
+  } else if(classTask === 'task') {
+    taskSelected.className = 'task-selected';
+  }
+}
+
+function markColor() {
+  let taskSelected = document.querySelector('div.my-tasks div');
+  console.log(taskSelected);
+  taskSelected.addEventListener('click', isSelected);
+}
+
+
+
+
+createDaysOfTheWeek();
+daysMonth();
+createButtonHoliday('Feriados');
+changeColor();
+createButtonFriday('Sexta-Feira');
+changeFriday();
+zoomList();
+addTask('Cozinhar');
+addColorTask('blue');
+markColor();
