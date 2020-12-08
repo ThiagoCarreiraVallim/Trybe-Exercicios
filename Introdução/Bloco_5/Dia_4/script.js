f1e914fd9defd94d554d0fc32f09de31c0f0109a
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2020-12-07 23:50:36 -0400

    Adiciona finalizaçoes

--- Introdução/Bloco_5/Dia_4/script.js
+++ Introdução/Bloco_5/Dia_4/script.js
@@ -14,57 +14,67 @@ const fontMono = document.querySelector('#monospace');
 const bodyWay = document.getElementsByTagName('body');
 
 function changeBkColor() {
+  bodyWay[0].style.backgroundColor = localStorage.getItem('background');
   btnBkColor.addEventListener('click', function() {
     bodyWay[0].style.backgroundColor = bkColor.value;
+    localStorage.setItem('background', bodyWay[0].style.backgroundColor);
   })
 }
 
 function changeFontColor() {
+  bodyWay[0].style.color = localStorage.getItem('fontColor')
   btnFontColor.addEventListener('click', function() {
     bodyWay[0].style.color = fontColor.value;
+    localStorage.setItem('fontColor', bodyWay[0].style.color);
   })
 }
 
 function changeFontSize() {
+  bodyWay[0].style.fontSize = localStorage.getItem('fontSize')
   btnUpFont.addEventListener('click', function() {
     let atual = comun + 1
     comun = atual;
     bodyWay[0].style.fontSize = `${atual}px`;
+    localStorage.setItem('fontSize', bodyWay[0].style.fontSize)
   })
   btnDownFont.addEventListener('click', function() {
     let decre = comun - 1;
     comun = decre;
     bodyWay[0].style.fontSize = `${decre}px`;
+    localStorage.setItem('fontSize', bodyWay[0].style.fontSize)
   })
 }
 
 function changeLineHeigth() {
+  bodyWay[0].style.lineHeight = localStorage.getItem('lineHeight');
   btnUpHeight.addEventListener('click', function() {
     let aument = height + 2;
     height = aument;
     bodyWay[0].style.lineHeight = `${aument}px`;
+    localStorage.setItem('lineHeight', bodyWay[0].style.lineHeight);
   })
   btnDownHeight.addEventListener('click', function() {
     let dimin = height - 2;
     height = dimin;
     bodyWay[0].style.lineHeight = `${dimin}px`;
+    localStorage.setItem('lineHeight', bodyWay[0].style.lineHeight);
   })
 }
 
 function changeFonteFamily() {
+  bodyWay[0].style.fontFamily = localStorage.getItem('fontFamily');
   fontArial.addEventListener('click', function() {
     bodyWay[0].style.fontFamily = 'arial';
+    localStorage.setItem('fontFamily', bodyWay[0].style.fontFamily);
   })
   fontTimes.addEventListener('click', function() {
     bodyWay[0].style.fontFamily = 'Times New Roman';
+    localStorage.setItem('fontFamily', bodyWay[0].style.fontFamily);
   })
   fontMono.addEventListener('click', function() {
     bodyWay[0].style.fontFamily = 'monospace';
+    localStorage.setItem('fontFamily', bodyWay[0].style.fontFamily);
   })
-}
-
-function addMemrise() {
-
 }
 
 changeBkColor();
