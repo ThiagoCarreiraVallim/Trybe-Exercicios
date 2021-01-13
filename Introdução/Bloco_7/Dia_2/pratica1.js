0226024a71b86a63f8ad0c01e95a765e48c68332
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-01-13 14:54:26 -0400

    Adiciona praticas das 5 partes

--- Introdução/Bloco_7/Dia_2/pratica1.js
+++ Introdução/Bloco_7/Dia_2/pratica1.js
@@ -1,20 +1,5 @@
-const customer1 = {
-  firstName: 'Roberto',
-  age: 22,
-  job: 'Teacher',
-};
-
-console.log(customer1);
-
-customer1.lastName = 'Faria';
-console.log(customer1);
-
-const customer2 = {
-  firstName: 'Maria',
-  age: 23,
-  job: 'Medic',
-};
-
-console.log(customer2);
-customer2['lastName'] = 'Silva';
-console.log(customer2);
+const obj = (objeto, chave, valor) => {
+  objeto[chave] = valor;
+  return objeto;
+}
+console.log(obj({}, 'nome', 'Thiago'));
--- Introdução/Bloco_7/Dia_2/pratica3.js
+++
@@ -1,5 +0,0 @@
-const obj = (objeto, chave, valor) => {
-  objeto[chave] = valor;
-  return objeto;
-}
-console.log(obj({}, 'nome', 'Thiago'));
