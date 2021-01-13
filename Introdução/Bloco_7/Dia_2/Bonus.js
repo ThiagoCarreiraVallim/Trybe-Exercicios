ad1dce3e17fd7b26ef99577bd41a7f815090acd4
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-01-13 18:09:56 -0400

    Adiciona Bonus

---
+++ Introdução/Bloco_7/Dia_2/Bonus.js
@@ -0,0 +1,49 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const lessons = function () {
+  const arrObj2 = [lesson1, lesson2, lesson3];
+  const arrObj = ['lesson1', 'lesson2', 'lesson3'];
+  const allLessons = {};
+  arrObj.forEach((value, index) => allLessons[value] = Object.assign({}, arrObj2[index]));
+  return allLessons
+}
+const allLessons = lessons();
+
+const mathStudents = obj => obj.lesson1.numeroEstudantes;
+//console.log(mathStudents(allLessons));
+
+const teachers = (obj, teacher) => {
+  const lessons = Object.values(obj);
+  const objeto = {professor: teacher};
+  const mat = [];
+  let alunos = 0;
+  lessons.forEach(obj => {
+    if (obj.professor === teacher) {
+      mat.push(obj.materia);
+      alunos += obj.numeroEstudantes;
+    }
+  });
+  objeto.aulas = mat;
+  objeto.estudantes = alunos;
+  return objeto;
+}
+
+console.log(teachers(allLessons, 'Maria Clara'));
