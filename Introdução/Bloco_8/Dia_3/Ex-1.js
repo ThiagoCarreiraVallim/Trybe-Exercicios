db626939302780f54811f2b57b2475281d911da3
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-01-20 22:28:48 -0400

    Adiciona exercicios do dia 3

---
+++ Introdução/Bloco_8/Dia_3/Ex-1.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+
+const arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+
+function flatten() {
+  // escreva seu código aqui
+  return arrays.reduce((acc, array) => {
+    array.forEach(value => {
+    acc.push(value);
+    })
+    return acc;
+  }, []);
+}
+
+assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
