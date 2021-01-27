ee8261734224828db2d01038d7d45530e6d7209f
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-01-27 10:09:00 -0400

    Adiciona exercicios do dia 9.1

---
+++ Introdução/Bloco_9/Dia_1/pratica1.js
@@ -0,0 +1,21 @@
+// setTimeout(() => {
+//   console.log('Comprar parafusos') // Comprar parafusos
+//   console.log('Adicionar ao estoque') // Adicionar ao estoque
+// }, 2000);
+
+// console.log('1 - Receber roda'); // 1 - Receber roda
+// console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
+// console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
+
+// for (let index = 0; index < 21; index += 1) {
+//   setTimeout(() => {
+//     console.log(index)
+//   }, index * 1000);
+// }
+
+// const alerta = setInterval(() => {
+//   console.log('Alerta!');
+// }, 500);
+
+// clearInterval(alerta, 5000);
+
